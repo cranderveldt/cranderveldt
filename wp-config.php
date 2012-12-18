79f045cd97f680d6d64bf954f50ac79daf15956c
Trevor Moore <syghefish@benetnasch.dreamhost.com>
2012-12-17 16:21:31 -0800

    shouldnt be much to add

--- wp-config.php
+++ wp-config.php
@@ -19,13 +19,13 @@
 define('DB_NAME', 'wp_cranderveldt');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'cranderveldtcom');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'GkehX8Me');
 
 /** MySQL hostname */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'mysql.cranderveldt.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
