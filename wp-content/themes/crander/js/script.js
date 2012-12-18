63f09e3feede658bea55b348d49e560e5ce92453
Trevor <djnaphouse@gmail.com>
2012-12-17 19:52:21 -0500

    fixed main nav styles

--- wp-content/themes/crander/js/script.js
+++ wp-content/themes/crander/js/script.js
@@ -3,6 +3,9 @@ jQuery(document).ready(function($){
   $('#menu a').each(function(){
     $(this).html('<span class="ss-icon ss-standard">' + $(this).attr('title') + '</span><span class="title">' + $(this).text() + '</span>');
   });
+  $('#menu a span.title').each(function(){
+    $(this).css('left', '-' + ((($(this).width() + 10)/2) + 14) + 'px');
+  });
   $('#footer-nav a').each(function(){
     $(this).html('<span class="ss-icon ss-standard">' + $(this).attr('title') + '</span>');
   });
