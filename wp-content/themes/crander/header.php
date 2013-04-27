6fad61a22cccf5234b1e222993dbd412e2ff709c
Trevor <djnaphouse@gmail.com>
2013-04-27 10:49:04 -0400

    updated styles

--- wp-content/themes/crander/header.php
+++ wp-content/themes/crander/header.php
@@ -45,29 +45,33 @@ if ($redirect)
     
 <body class="body-<?php the_ID(); ?>">
   <section id="container" class="container-fluid">
-    <header class="row-fluid">
-      <div class="span5 logo">
-        <h1><a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a></h1>
-        <small><?php bloginfo('description'); ?></small>
-      </div>
-      <div class="span7 navigation">
-        <nav id="menu" class="clearfix">
-          <?php wp_nav_menu(array( 
-            'theme_location'  => 'menu-1',
-            'menu'            => '', 
-            'container'       => false,
-            'menu_class'      => 'menu row-fluid', 
-            'echo'            => true,
-            'fallback_cb'     => 'wp_page_menu',
-            'before'          => '',
-            'after'           => '',
-            'link_before'     => '',
-            'link_after'      => '',
-            'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-            'depth'           => 0,
-            'walker'          => ''
-          )); ?>
-        </nav>
+    <header>
+      <div class="max1400">
+        <div class="row-fluid">
+          <div class="span5 logo">
+            <h1><a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a></h1>
+            <small><?php bloginfo('description'); ?></small>
+          </div>
+          <div class="span7 navigation">
+            <nav id="menu" class="clearfix">
+              <?php wp_nav_menu(array( 
+                'theme_location'  => 'menu-1',
+                'menu'            => '', 
+                'container'       => false,
+                'menu_class'      => 'menu row-fluid', 
+                'echo'            => true,
+                'fallback_cb'     => 'wp_page_menu',
+                'before'          => '',
+                'after'           => '',
+                'link_before'     => '',
+                'link_after'      => '',
+                'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                'depth'           => 0,
+                'walker'          => ''
+              )); ?>
+            </nav>
+          </div>
+        </div>
       </div>
     </header>
-    <section id="content">
+    <section id="content" class="max1400">
