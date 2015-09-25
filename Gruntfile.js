9e211512eea96f7f6c16fbcee6db3035c77f0d66
cranderveldt <djnaphouse@gmail.com>
2015-09-25 10:20:04 -0400

    flexbox :))))))

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,30 @@
 module.exports = function(grunt) {
-    "use strict";
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        
-        sass: {
-            dist: {
-                options: {
-                    style: 'compressed',
-                    debugInfo: false,
-                    lineNumbers: false
-                },
-                files: {
-                    'wp-content/themes/crander/style.css': 'wp-content/themes/crander/sass/style.scss', // 'destination': 'source'
-                }
-            }
-        },
-        
-        // This sets up watch configurations
-        watch: {
-            all: {
-                //files: ['sass/*.scss', 'dev/*.js'],
-                files: ['wp-content/themes/crander/sass/*.scss'],
-                //tasks: ['common','sass:dist']
-                tasks: ['sass:dist']
-            }
-        },
-    });
-    
-    // Load the grunt plugins
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-sass');
 
-    // Default task(s).
-    grunt.registerTask("default", ['sass:dist', 'watch:all']);
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    sass: {
+      dev: {
+        options: {
+          style: 'compressed'
+        },
+        files : {
+          'wp-content/themes/crander/style.css': 'wp-content/themes/crander/sass/style.scss'
+        }
+      }
+    },
+    watch: {
+      sass: {
+        files: ['wp-content/themes/crander/sass/*.scss'],
+        tasks: ['sass:dev']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  // Default task(s).
+  grunt.registerTask('default', ['sass:dev','watch:sass']);
+
 };
